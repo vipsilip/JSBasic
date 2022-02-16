777c6e86402ada281ebeed2fbe58d7c469785cdd
vipsilip <34795665+vipsilip@users.noreply.github.com>
2022-02-16 16:40:12 +0700

    update

--- src/05-01-bt.js
+++
@@ -1,7 +0,0 @@
-function getTicketPrice(age) {
-    if (age <= 0 || age > 125) return -1;
-    if (age < 6 || age >= 70) return 0;
-    if (6 <= age && age <= 12) return 20000;
-    if (12 < age && age < 70) return 50000;
-  }
- 
